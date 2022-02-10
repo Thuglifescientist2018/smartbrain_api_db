e274f215ccff3e460895c99f3aee6ada4cb7ff4d
= <=>
2022-02-10 11:57:08 +0545

    transaction register and login

--- server.js
+++ server.js
@@ -75,17 +75,35 @@ let id = 125;
 app.post('/register', (req, res) => {
     
     const {email, name, password} = req.body;
-    bcrypt.hash(password, null, null, function(err, hash) {
-        // Store hash in your password DB.
-        // console.log(hash)
-    });
-        db('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
-        }).then(user => res.json(user[0])).catch(err  => res.status(400).json("unable to register"))
+    const hash = bcrypt.hashSync(password);
+
+    db.transaction(trx => {
+        trx.insert({
+          hash: hash,
+          email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+              // loginEmail[0] --> this used to return the email
+              // TO
+              // loginEmail[0].email --> this now returns the email
+              email: loginEmail[0].email,
+              name: name,
+              joined: new Date()
+            })
+            .then(user => {
+              res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+      })
+      .catch(err => res.status(400).json('unable to register'))
        
         
         id++;
